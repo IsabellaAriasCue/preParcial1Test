b77f0bd874eabe85e05d5022c04f6b60487ab346
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-22 11:52:08 -0500

    RED para RF2

--- test/test_notas.py
+++ test/test_notas.py
@@ -23,3 +23,15 @@ def test_registrar_nota_invalida_mayor_a_cinco():
 
     with pytest.raises(ValueError):
         estudiante.registrar_nota("Matematicas", "2025-1", 6.0)
+
+#RF2
+
+def test_aprueba_con_nota_tres():
+    estudiante = Estudiante()
+
+    assert estudiante.aprobo(3.0) is True
+
+def test_reprueba_con_nota_menor_a_tres():
+    estudiante = Estudiante()
+
+    assert estudiante.aprobo(2.9) is False
